9917ac6d85c07b694578187c0b6b36810e18492c
tbeeler <tbeeler@apple.com>
2015-07-24 12:35:16 -0700

    made nodes dynamic based on role

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,16 +4,37 @@
 #
 # Copyright (c) 2015 Tim Beeler, All Rights Reserved.
 
-node.default['haproxy']['members'] =[{
-  "hostname" => "ec2-54-84-204-79.compute-1.amazonaws.com",
-  "ipaddress" => "ec2-54-84-204-79.compute-1.amazonaws.com",
-  "port" => 80,
-  "ssl_port" => 80
-  },{
-  "hostname" => "ec2-52-7-171-167.compute-1.amazonaws.com",
-  "ipaddress" => "ec2-52-7-171-167.compute-1.amazonaws.com",
-  "port" => 80,
-  "ssl_port" => 80
-    }]
+# old school 
+#node.default['haproxy']['members'] =[{
+#  
+#"hostname" => "ec2-54-84-204-79.compute-1.amazonaws.com",
+  #"ipaddress" => "ec2-54-84-204-79.compute-1.amazonaws.com",
+  #"port" => 80,
+  #"ssl_port" => 80
+  #},{
+  #"hostname" => "ec2-52-7-171-167.compute-1.amazonaws.com",
+  #"ipaddress" => "ec2-52-7-171-167.compute-1.amazonaws.com",
+  #"port" => 80,
+  #"ssl_port" => 80
+  #  }]
 
-  include_recipe "haproxy::default"
+# dynamic version
+#knife search for nodes "role:web"
+all_web_nodes = search("node","role:web") # node_oject 
+
+members =[]
+
+#convert each web node object to hash with the hostname, ipaddress, port, and ssl_port
+all_web_nodes.each do |web_node|
+  member={
+    "hostname" => web_node["cloud"]["public_hostname"],
+    "ipaddress" => web_node["cloud"]["public_ipv4"],
+    "port" => 80,
+    "ssl_port" => 80
+  }
+  members.push(member)
+end
+
+node.default['haproxy']['members'] = members
+ 
+include_recipe "haproxy::default"
